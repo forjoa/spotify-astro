c0e82063043ff7571b149080c3655a994e763b92
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-09-01 18:52:19 +0200

    build: add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), react()],
   output: 'hybrid'
 });
