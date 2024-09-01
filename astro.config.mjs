fbb24edb36dba61704b599842e64a17bd351d946
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-09-01 19:07:12 +0200

    build: install dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'hybrid'
+  output: 'server'
 });
